50facb885305fa9eecd99aec73873d66ceaa0b6e
nataliasenchkovskaia <nataliavino13@gmail.com>
2023-12-04 18:36:20 -0500

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -136,3 +136,17 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# smtp config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
